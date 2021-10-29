4bf31c5e09b8c358ad3e11da16aa2273e77294e5
Ram Cheekoti <ramcheekoti.hyd@gmail.com>
2021-10-29 11:14:34 +0530

    added the ec2 instance and provider files for alias and multifpile provider concept

---
+++ instance.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "webfromeast" {
+  ami           = "ami-09e67e426f25ce0d7" # this is UBUNTU 20 id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Node from Default Region - East "
+
+  }
+
+}
+
+resource "aws_instance" "webfromwest" {
+  ami           = "ami-0d382e80be7ffdae5" # this is UBUNTU 20 id
+  instance_type = "t2.micro"
+  provider      = aws.west
+
+  tags = {
+    Name = "Node from West Region "
+
+  }
+
+}
